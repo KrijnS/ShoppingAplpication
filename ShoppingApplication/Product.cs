f01afc5b3e48c2b3d71f70e57d89929ecbab2cf9
Krijn van der Steen <krijnvdsteen@live.nl>
2019-10-31 09:25:24 +0100

    Add ShoppingFee implementation

--- ShoppingApplication/Product.cs
+++ ShoppingApplication/Product.cs
@@ -8,5 +8,25 @@ namespace ShoppingApplication
 {
     abstract class Product
     {
+        string name;
+        int productPrice;
+        int weight;
+
+        public Product(string name, int productPrice, int weight)
+        {
+            this.name = name;
+            this.productPrice = productPrice;
+            this.weight = weight;
+        }
+
+        public abstract string TransportMethod();
+
+        public int TotalPrice(OrderType orderType)
+        {
+            ShippingFee shippingFee = new ShippingFee();
+            int totalPrice;
+            totalPrice = this.productPrice + shippingFee.CalculateFee(this.weight, orderType);
+            return totalPrice;
+        }
     }
 }
--- ShoppingApplication/ShippingFee.cs
+++ ShoppingApplication/ShippingFee.cs
@@ -8,5 +8,19 @@ namespace ShoppingApplication
 {
     class ShippingFee
     {
+        public int CalculateFee(int weight, OrderType orderType)
+        {
+            float fee;
+            if (orderType.Equals("Inland"))
+            {
+                fee = 2;
+            }
+            else
+            {
+                fee = 5;
+            }
+            fee = fee * (weight / 100);
+            return (int) fee;
+        }
     }
 }
