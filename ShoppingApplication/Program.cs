ab6c97683e51aa16df769757239badfa997ee3d1
Krijn van der Steen <krijnvdsteen@live.nl>
2019-11-04 21:55:35 +0100

    Add basic shopping cart implementation

--- ShoppingApplication/Program.cs
+++ ShoppingApplication/Program.cs
@@ -10,12 +10,20 @@ namespace ShoppingApplication
     {
         static void Main(string[] args)
         {
-            Address addresss = new Address(null, null, null, null, OrderType.Inland);
-            Address add = addresss.GetAddress();
+            //Address addresss = new Address(null, null, null, null, OrderType.Inland);
+            //Address add = addresss.GetAddress();
 
 
             // digital = new DigitalProduct("productX", 10, 10, "Goed product");
             //Console.WriteLine(digital.TransportMethod());
+            PhysicalProduct product = new PhysicalProduct("Laptop", 300, 10, "Mooi ding");
+            DigitalProduct product2 = new DigitalProduct("Software", 50, 0, "Mooi software");
+
+            Dictionary<Product, int> amountOfProduct = new Dictionary<Product, int>();
+            ShoppingCart shoppingCart = new ShoppingCart(amountOfProduct);
+            shoppingCart.AddToCart(product,10);
+            shoppingCart.AddToCart(product2,5);
+            shoppingCart.DisplayCart();
 
             CustomerInterface customerInterface = new CustomerInterface();
             customerInterface.ShowAllProducts();
--- ShoppingApplication/ShoppingCart.cs
+++ ShoppingApplication/ShoppingCart.cs
@@ -8,5 +8,31 @@ namespace ShoppingApplication
 {
     class ShoppingCart
     {
+        Dictionary<Product, int> amountOfProduct;
+
+        public ShoppingCart(Dictionary<Product, int> amountOfProduct)
+        {
+            this.amountOfProduct = amountOfProduct;
+        }
+
+        public void AddToCart(Product product, int amount)
+        {
+            if (!(this.amountOfProduct.Count == 0) && this.amountOfProduct.TryGetValue(product, out int amountStored))
+            {
+                this.amountOfProduct[product] = amount + amountStored;
+            }
+            else
+            {
+                this.amountOfProduct.Add(product, amount);
+            }          
+        }
+
+        public void DisplayCart()
+        {
+            foreach(KeyValuePair<Product,int> cartProduct in amountOfProduct)
+            {
+                Console.WriteLine(cartProduct.Value + "x " + cartProduct.Key.name);
+            }
+        }
     }
 }
