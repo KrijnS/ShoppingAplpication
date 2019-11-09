a6f1a155678c8ee0d9526f86e8d62ff5b9a45730
Krijn van der Steen <krijnvdsteen@live.nl>
2019-11-09 14:51:33 +0100

    Add first tests, ZeroQuantity() is not yet completed

---
+++ ShoppingApplicationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingApplicationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ShoppingApplicationTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("571114f0-30b8-449c-bc95-ad13f90645a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ShoppingApplicationTests/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingApplication;
+
+namespace ShoppingApplicationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NoShippingForDigital()
+        {
+            string[] allLines = File.ReadAllLines("Database.txt", Encoding.UTF8);
+            int[] allProducts = new int[allLines.Length];
+            ShoppingCart shoppingCart = new ShoppingCart(allProducts);
+            shoppingCart.AddToCart(0, 8);
+            shoppingCart.AddToCart(4, 10);
+            Parser parser = new Parser();
+            DigitalProduct one = (DigitalProduct) parser.GetProductFromIndex(0);
+            DigitalProduct two = (DigitalProduct) parser.GetProductFromIndex(4);
+            int expectedPrice = 8 * one.productPrice + 10 * two.productPrice;
+
+            Assert.IsTrue(expectedPrice == shoppingCart.PriceOfCart(OrderType.Inland));
+        }
+
+        [TestMethod]
+        public void ShippingForPhysical()
+        {
+            string[] allLines = File.ReadAllLines("Database.txt", Encoding.UTF8);
+            int[] allProducts = new int[allLines.Length];
+            ShoppingCart shoppingCart = new ShoppingCart(allProducts);
+            shoppingCart.AddToCart(1, 8);
+            shoppingCart.AddToCart(4, 10);
+            Parser parser = new Parser();
+            PhysicalProduct one = (PhysicalProduct)parser.GetProductFromIndex(1);
+            DigitalProduct two = (DigitalProduct)parser.GetProductFromIndex(4);
+            int noShippingPrice = 8 * one.productPrice + 10 * two.productPrice;
+
+            Assert.IsTrue(noShippingPrice < shoppingCart.PriceOfCart(OrderType.Inland));
+        }
+
+        [TestMethod]
+        public void NoZeroQuantity()
+        {
+            string[] allLines = File.ReadAllLines("Database.txt", Encoding.UTF8);
+            int[] allProducts = new int[allLines.Length];
+            ShoppingCart shoppingCart = new ShoppingCart(allProducts);
+            shoppingCart.AddToCart(1, 8);
+            shoppingCart.AddToCart(1, -8);
+            
+        }
+
+        [TestMethod]
+        public void InlandCheaperThenAbroad()
+        {
+            string[] allLines = File.ReadAllLines("Database.txt", Encoding.UTF8);
+            int[] allProducts = new int[allLines.Length];
+            ShoppingCart shoppingCart = new ShoppingCart(allProducts);
+            shoppingCart.AddToCart(1, 8);
+            Assert.IsTrue(shoppingCart.PriceOfCart(OrderType.Inland) < shoppingCart.PriceOfCart(OrderType.Abroad));
+        }
+
+    }
+}
